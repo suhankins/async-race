1802ee9068ffdb77c365d204dddf2fe7e0d5e54f
Dimitri Sukhankin <punishedbernadetta@niuitmo.ru>
2023-06-15 09:51:23 +0400

    made useClickPosition support dragging

--- src/components/GarageList/RGBPallete/hooks/useClickPosition.ts
+++ src/components/GarageList/RGBPallete/hooks/useClickPosition.ts
@@ -1,17 +1,54 @@
-import { useCallback, useState } from 'react';
+import { RefObject, useEffect, useState } from 'react';
 
-export function useClickPosition() {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const onClickHandler = useCallback(
-        (e: React.MouseEvent<HTMLDivElement>) => {
-            const target = e.target as HTMLDivElement;
-            const rect = target.getBoundingClientRect();
-            const x = (e.clientX - rect.left) / rect.width;
-            const y = (e.clientY - rect.top) / rect.height;
-            console.log(x, y);
-            setPosition({ x, y });
-        },
-        []
-    );
-    return [position, onClickHandler] as const;
+function getMousePosition(e: MouseEvent, ref: RefObject<HTMLDivElement>) {
+    if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        let x = (e.clientX - rect.left) / rect.width;
+        let y = (e.clientY - rect.top) / rect.height;
+
+        if (x < 0) x = 0;
+        if (x > 1) x = 1;
+        if (y < 0) y = 0;
+        if (y > 1) y = 1;
+
+        return { x, y };
+    }
+    return { x: 0, y: 0 };
+}
+
+/**
+ *
+ * @returns
+ */
+export function useClickPosition(
+    ref: RefObject<HTMLDivElement>,
+    defaultPosition = { x: 0, y: 0 }
+) {
+    const [position, setPosition] = useState(defaultPosition);
+    useEffect(() => {
+        let mouseDown = false;
+        const handleMouseDown = (e: MouseEvent) => {
+            if (ref.current) {
+                mouseDown = true;
+                setPosition(getMousePosition(e, ref));
+            }
+        };
+        const handleMouseMove = (e: MouseEvent) => {
+            if (mouseDown && ref.current) {
+                setPosition(getMousePosition(e, ref));
+            }
+        };
+        const handleMouseUp = () => {
+            mouseDown = false;
+        };
+        ref.current?.addEventListener('mousedown', handleMouseDown);
+        ref.current?.addEventListener('mousemove', handleMouseMove);
+        ref.current?.addEventListener('mouseup', handleMouseUp);
+        return () => {
+            ref.current?.removeEventListener('mousedown', handleMouseDown);
+            ref.current?.removeEventListener('mousemove', handleMouseMove);
+            ref.current?.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, [ref]);
+    return [position] as const;
 }
