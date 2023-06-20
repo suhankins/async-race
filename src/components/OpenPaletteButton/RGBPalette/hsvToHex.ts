a9265184ee23350c01ddbb554e9f953ea315a926
Dimitri Sukhankin <punishedbernadetta@niuitmo.ru>
2023-06-20 12:32:14 +0400

    refactoried hsvToHex to use tinyColor

--- src/components/OpenPaletteButton/RGBPalette/hsvToHex.ts
+++ src/components/OpenPaletteButton/RGBPalette/hsvToHex.ts
@@ -1,3 +1,5 @@
+import tinyColor from 'tinycolor2';
+
 /**
  * Converts hex to HSV
  * @param {string} hex string like '#ff0000'
@@ -8,91 +10,23 @@ export function hexToHsv(hex: string): {
     saturation: number;
     value: number;
 } {
-    // Remove the leading '#' if present
-    hex = hex.replace(/^#/, '');
-
-    // Extract the RGB values from the hex string
-    const r = parseInt(hex.slice(0, 2), 16) / 255;
-    const g = parseInt(hex.slice(2, 4), 16) / 255;
-    const b = parseInt(hex.slice(4, 6), 16) / 255;
-
-    // Find the maximum and minimum values
-    const max = Math.max(r, g, b);
-    const min = Math.min(r, g, b);
-    const delta = max - min;
-
-    let h = 0;
-    let s = 0;
-    let v = max;
-
-    // Calculate the hue
-    if (delta !== 0) {
-        if (max === r) {
-            h = ((g - b) / delta) % 6;
-        } else if (max === g) {
-            h = (b - r) / delta + 2;
-        } else {
-            h = (r - g) / delta + 4;
-        }
-        h *= 60;
-    }
-
-    // Calculate the saturation
-    if (max !== 0) {
-        s = delta / max;
-    }
-    return { hue: h / 360, saturation: s, value: v };
-}
-
-function getHex(number: number) {
-    return Math.round(number * 255)
-        .toString(16)
-        .padStart(2, '0');
+    const color = tinyColor(hex);
+    const { h: hue, s: saturation, v: value } = color.toHsv();
+    return { hue, saturation, value };
 }
 
 /**
  * Converts HSV to hex
- * @param {number} h hue from 0 to 1
- * @param {number} s saturation from 0 to 1
- * @param {number} v value from 0 to 1
+ * @param {number} hue hue from 0 to 1
+ * @param {number} saturation saturation from 0 to 1
+ * @param {number} value value from 0 to 1
  * @returns
  */
-export function hsvToHex(h: number, s: number, v: number): string {
-    // Normalize the values
-    const normalizedH = Math.max(0, Math.min(359, h * 360));
-    const normalizedS = Math.max(0, Math.min(1, s));
-    const normalizedV = Math.max(0, Math.min(1, v));
-
-    const chroma = normalizedV * normalizedS;
-    const x = chroma * (1 - Math.abs(((normalizedH / 60) % 2) - 1));
-    const minimumValue = normalizedV - chroma;
-    let r = 0;
-    let g = 0;
-    let b = 0;
-
-    if (normalizedH >= 0 && normalizedH < 60) {
-        r = chroma;
-        g = x;
-    } else if (normalizedH >= 60 && normalizedH < 120) {
-        r = x;
-        g = chroma;
-    } else if (normalizedH >= 120 && normalizedH < 180) {
-        g = chroma;
-        b = x;
-    } else if (normalizedH >= 180 && normalizedH < 240) {
-        g = x;
-        b = chroma;
-    } else if (normalizedH >= 240 && normalizedH < 300) {
-        r = x;
-        b = chroma;
-    } else if (normalizedH >= 300 && normalizedH < 360) {
-        r = chroma;
-        b = x;
-    }
-
-    const hexR = getHex(r + minimumValue);
-    const hexG = getHex(g + minimumValue);
-    const hexB = getHex(b + minimumValue);
-
-    return `#${hexR}${hexG}${hexB}`;
+export function hsvToHex(
+    hue: number,
+    saturation: number,
+    value: number
+): string {
+    const color = tinyColor({ h: hue * 360, s: saturation, v: value });
+    return color.toHexString();
 }
